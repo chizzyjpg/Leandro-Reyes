1bbe6c210e1fbf4638f4774b04f1a7c57323f7da
chizzyjpg <64444067+chizzyjpg@users.noreply.github.com>
2020-04-29 21:58:33 -0300

    Add files via upload

---
+++ ejercicio2.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Dim contador#
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAñadir.Click
+        Dim num As Integer
+        num = Val(txtNumero.Text)
+        If txtNumero.Text > -1 And txtNumero.Text < 101 Then
+            ListBox1.Items.Add(txtNumero.Text)
+            contador += Val(txtNumero.Text)
+            txtPorcentaje.Text = Math.Round(contador / Val(ListBox1.Items.Count), 2)
+            'MsgBox(contador)
+        Else
+            MsgBox("Ingrece un número entre 1 y 100")
+        End If
+        txtNumero.Clear()
+        txtNumero.Focus()
+    End Sub
+End Class
